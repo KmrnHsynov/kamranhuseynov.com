dad48930fbaf1776f87ef08dde3dc873c6e1c3e0
Kamran <huseynovkamran07@gmail.com>
2026-04-22 11:07:45 +0400

    fix: update databaseURL in Firebase config for correct endpoint

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@
 const FIREBASE_CONFIG = {
   apiKey:            "AIzaSyB_wsJu9BF5FBPkR9tMC0dT8pPPsvXgdxw",
   authDomain:        "quiz-az-73c96.firebaseapp.com",
-  databaseURL:       "https://quiz-az-73c96-default-rtdb.firebaseio.com",
+  databaseURL:       "https://quiz-az-73c96-default-rtdb.europe-west1.firebasedatabase.app",
   projectId:         "quiz-az-73c96",
   storageBucket:     "quiz-az-73c96.firebasestorage.app",
   messagingSenderId: "443509521109",
